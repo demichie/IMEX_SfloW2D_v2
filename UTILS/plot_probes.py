304e1d713db3b858a35658bcd283a5c0ba819f3a
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-10-27 15:56:29 +0100

    Update plot_probes.py

--- UTILS/plot_probes.py
+++ UTILS/plot_probes.py
@@ -31,25 +31,29 @@ content = file.readlines()
 
 vars = content[1].split(',')[:]
 
-# print('vars',vars)
+print('vars',vars)
 
 # print("variable units line")
 
 units = content[2].split(',')[:]
+print()
+
 
 
 for i in range(len(vars)-1):
 
     # print(i,vars[i+1])
 
-    plt.subplots(figsize=(10, 6))
+    plt.subplots(figsize=(10, 4))
+    plt.rcParams.update({'font.size': 14})
     plt.plot(data[:,0],data[:,i+1])
     plt.xlim((np.amin(data[:,0]),np.amax(data[:,0])))
     
     plt.xlabel('Time (s)')
     plt.ylabel(vars[i+1]+' '+units[i+1])
+    plt.tight_layout()
     # print((vars[i+1].strip()).split(' ')[0]+'.png')
-    fig_name = prb_file.replace('.csv','_'+(vars[i+1].strip().replace('.','_'))+'.png')
+    fig_name = prb_file.replace('.csv','_'+(vars[i+1].strip().replace('.','_'))+'.pdf')
     fig_name = fig_name.replace('_.','.')
     plt.savefig(fig_name)
 
